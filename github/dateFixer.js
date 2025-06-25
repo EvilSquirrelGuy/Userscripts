6f484e44d460d430bfd076d45c42d14e0781b7ce
Evil Squirrel <evil.squirrelo.xtreme@gmail.com>
2025-06-25 22:18:29 +0100

    Create dateFixer.js

---
+++ github/dateFixer.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         GitHub Timestamp Format Fixer
+// @namespace    http://tampermonkey.net/
+// @version      2025-06-25
+// @description  Replaces timestamps on GitHub with d/m/y formatted dates and 24h time
+// @author       EvilSquirrelGuy
+// @match        https://github.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=hibbard.eu
+// @grant        none
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+function fixDates() {
+    'use strict';
+    // find all the times
+    let relativeTimes = document.getElementsByTagName("relative-time");
+
+    for (let rt of relativeTimes) {
+      // cast to time
+      const date = new Date(rt.datetime);
+      // make a short dd MMM date
+      const shortFmt = `${date.toLocaleDateString("en-GB", {day: "2-digit", month: "short"})}`;
+
+      // replace the hover title
+      rt.title = date.toLocaleString("en-GB", {dateStyle: "medium" ,timeStyle: "long"});
+
+      // check the special hidden stuff
+      let shadowText = rt.shadowRoot?.textContent;
+      // if it's an 'on' date, replace element contents
+      if (shadowText.startsWith("on")) {
+        let hasYear = /\d{4}$/.test(shadowText);
+        // replace contents, optionally adding year if it was originally present
+        rt.shadowRoot.textContent = "on " + shortFmt + (hasYear ? ` ${date.getFullYear()}` : "");
+      }
+    }
+}
+
+
+// run initially
+fixDates();
+
+// then watch for new stuff being added
+const observer = new MutationObserver(() => {
+    fixDates(); // reapply formatting whenever DOM changes
+});
+
+// watch for DOM changes so timestamps can instantly be fixed again
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
